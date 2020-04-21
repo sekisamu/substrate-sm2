40b803ee44785df06173059e42de7447bd9f17b4
hammeWang <dsw0602@foxmail.com>
2020-04-21 17:56:41 +0800

    modify example in utils

--- utils/src/main.rs
+++ utils/src/main.rs
@@ -4,40 +4,53 @@ use substrate_subxt::{
 	balances,
 	Error,
 	KusamaRuntime,
+	system::System,
+	ExtrinsicSuccess,
 };
 
 use sp_core::crypto::{Pair as PairT, Public as PublicT, AccountId32};
 use crypto_sm2::sm2;
 
+type AccountId = <KusamaRuntime as System>::AccountId;
+type Balance = <KusamaRuntime as balances::Balances>::Balance;
 
 
 fn main() {
-	async_std::task::block_on(async move {
-		env_logger::init();
+	let result: Result<ExtrinsicSuccess<_>, Box<dyn std::error::Error + 'static>> =
+		async_std::task::block_on(async move {
+			env_logger::init();
 
-		let xt_result = transfer_balance().await;
-		match xt_result {
-			Ok(hash) => println!("Balance transfer extrinsic submitted: {}", hash),
-			Err(_) => eprintln!("Balance transfer extrinisic failed"),
+			let signer = sm2::Pair::from_string("//Test", None).expect("static values are valid; qed");
+			let dest = sm2::Pair::from_string("//Test1", None).expect("static values are valid; qed").public();
+
+
+			let cli = substrate_subxt::ClientBuilder::<KusamaRuntime>::new()
+				.build()
+				.await?;
+			let xt = cli.xt(signer, None).await?;
+			let xt_result = xt
+				.watch()
+				.events_decoder(|decoder| {
+					// for any primitive event with no type size registered
+					decoder.register_type_size::<(u64, u64)>("IdentificationTuple")
+				})
+				.submit(balances::transfer::<KusamaRuntime>(dest.clone().into_b32().into(), 10_000))
+				.await?;
+			Ok(xt_result)
+		});
+	match result {
+		Ok(extrinsic_success) => {
+			match extrinsic_success
+				.find_event::<(AccountId, AccountId, Balance)>("Balances", "Transfer")
+				{
+					Some(Ok((_from, _to, value))) => {
+						println!("Balance transfer success: value: {:?}", value)
+					}
+					Some(Err(err)) => println!("Failed to decode code hash: {}", err),
+					None => println!("Failed to find Balances::Transfer Event"),
+				}
 		}
-	});
+		Err(err) => println!("Error: {:?}", err),
+	}
 }
 
-
-async fn transfer_balance() -> Result<sp_core::H256, Error> {
-	// generate sm2 key-pair
-	let signer = sm2::Pair::from_string("//Test", None).expect("static values are valid; qed");
-	let dest = sm2::Pair::from_string("//Test1", None).expect("static values are valid; qed").public();
-
-	// note use of `KusamaRuntime`
-	substrate_subxt::ClientBuilder::<KusamaRuntime>::new()
-		.build()
-		.await?
-		.xt(signer, None)
-		.await?
-		.submit(balances::transfer::<KusamaRuntime>(
-			(dest.clone().into_b32()).into(),
-			10_000,
-		))
-		.await
-}
